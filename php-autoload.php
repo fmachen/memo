41c2cb2fa1543ad9c58bcac87602715c06b0a118
fmachen <florent.machen@gmail.com>
2015-04-29 22:40:27 +0200

    Create php-autoload.php

---
+++ php-autoload.php
@@ -0,0 +1,15 @@
+// PSR-0 compliant autoloader
+// http://www.php-fig.org/psr/psr-0/
+spl_autoload_register(function($className){
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strripos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
+});
